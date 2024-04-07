7247c43a871b1e7e766f15ee3b0a28a7a18fe5f2
kotenkoI <iryna.herashchenko@gmail.com>
2024-04-07 21:05:45 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,28 @@
 import iziToast from 'izitoast'; // Описаний у документації
 import 'izitoast/dist/css/iziToast.min.css'; // Додатковий імпорт стилів
-
 import SimpleLightbox from 'simplelightbox'; // Описаний у документації
 import 'simplelightbox/dist/simple-lightbox.min.css'; // Додатковий імпорт стилів
-
 import { getImages } from './js/pixabay-api';
 import { renderGallery } from './js/render-functions';
-
 export const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
-
 export const refs = {
   formEl: document.querySelector('.form'),
   gallery: document.querySelector('.gallery'),
   loaderEl: document.querySelector('.loader'),
   btnLoadMore: document.querySelector('.loadMore'),
 };
-
 let searchQuery;
 let currentPage = 1;
 let maxPage = 0;
 const perPage = 15;
-
 refs.formEl.addEventListener('submit', onFormSubmit);
 refs.btnLoadMore.addEventListener('click', onLoadMoreClick);
-
 deleteLoader();
 hideLoadMore();
-
 async function onFormSubmit(e) {
   e.preventDefault();
   createLoader();
@@ -38,7 +30,6 @@ async function onFormSubmit(e) {
   searchQuery = e.target.elements.request.value.trim();
   refs.gallery.innerHTML = '';
   currentPage = 1;
-
   if (!searchQuery) {
     deleteLoader();
     hideLoadMore();
@@ -48,7 +39,6 @@ async function onFormSubmit(e) {
     });
     return;
   }
-
   try {
     const data = await getImages(searchQuery, currentPage);
     console.log(data);
@@ -66,21 +56,7 @@ async function onFormSubmit(e) {
       return;
     }
     maxPage = Math.ceil(data.totalHits / perPage);
-     if (data.hits.length < perPage) {
-      console.log(data.hits.length )
-      console.log(perPage)
-      console.log(data.hits.length < perPage)
-    renderGallery(data.hits);
-    showLoadMore();
-  } catch (err) {
-    console.log(err);
-  }
-
-  deleteLoader();
-  // checkBtnStatus();
-  refs.formEl.reset();
-}
-if (data.hits.length < perPage) {
+    if (data.hits.length < perPage) {
       console.log(data.hits.length )
       console.log(perPage)
       console.log(data.hits.length < perPage)
@@ -88,6 +64,15 @@ if (data.hits.length < perPage) {
       hideLoadMore();
       return
     }
+    renderGallery(data.hits);
+    showLoadMore();
+  } catch (err) {
+    console.log(err);
+  }
+  deleteLoader();
+  // checkBtnStatus();
+  refs.formEl.reset();
+}
 async function onLoadMoreClick() {
   currentPage += 1;
   createLoader();
@@ -97,19 +82,16 @@ async function onLoadMoreClick() {
   } catch (err) {
     console.log(err);
   }
-
   myScroll();
   checkBtnStatus();
   deleteLoader();
 }
-
 function showLoadMore() {
   refs.btnLoadMore.classList.remove('is-hidden');
 }
 function hideLoadMore() {
   refs.btnLoadMore.classList.add('is-hidden');
 }
-
 function checkBtnStatus() {
   if (currentPage >= maxPage) {
     hideLoadMore();
@@ -117,23 +99,18 @@ function checkBtnStatus() {
       message: "We're sorry, but you've reached the end of search results.",
       position: 'topRight',
     });
-  }
-  else {
+  } else {
     showLoadMore();
   }
 }
-
 function deleteLoader() {
   refs.loaderEl.classList.add('is-hidden');
 }
-
 function createLoader() {
   refs.loaderEl.classList.remove('is-hidden');
 }
-
 function myScroll() {
   const height = refs.gallery.firstChild.getBoundingClientRect().height;
-
   scrollBy({
     top: height * 2,
     behavior: 'smooth',
